35c1d3d13901ec5865a24e1928bbc8d70288e3be
juanfrc <juanfrc2@gmail.com>
2018-06-22 19:33:41 -0500

    initial state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.changeTitle = this.changeTitle.bind(this)
+
+    this.state = {
+      title: "Hola Mundo desde Estado"
+    }
+  };
+
   render() {
     return (
-      <h1>Hola Mundo</h1>
+      <div>
+        <h1>{this.state.title}</h1>
+        <button onClick={this.changeTitle.bind(this)}>Cambie el titulo</button>
+      </div>
     );
+  }
+
+  changeTitle() {
+    this.setState ({
+      title: 'Nuevo titulo'
+    });
   }
 }
 
