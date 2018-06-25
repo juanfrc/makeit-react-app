3755d779f599e869677720e75e518d0e318c7a17
juanfrc <juanfrc2@gmail.com>
2018-06-24 23:05:41 -0500

    react plus bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div>
         <h1>Hola Mundo</h1>
+        <Button bsStyle="primary">Primary</Button>
       </div>
     )
   }
