edb8f99cc217267bccccd077b44329f5b54082cc
juanfrc <juanfrc2@gmail.com>
2018-06-24 22:54:34 -0500

    axios tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      names: []
+    }
+
+    axios.get(" http://localhost:3001/names")
+      .then(response => {
+        this.setState({
+          names: response.data
+        })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
   render() {
     return (
       <div>
         <h1>Hola Mundo</h1>
+        <ul>
+          {this.state.names.map(name =>
+            <li key={name}>{name}</li>
+          )}
+        </ul>
       </div>
     )
   }
