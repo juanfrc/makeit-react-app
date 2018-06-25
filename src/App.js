136b6d3e3c6e5f74452d1819199dac23640e107d
juanfrc <juanfrc2@gmail.com>
2018-06-24 22:23:42 -0500

    initial state

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <h1>Hola Mundo</h1>
-    );
+      <div>
+        <h1>Hola Mundo</h1>
+      </div>
+    )
   }
 }
 
